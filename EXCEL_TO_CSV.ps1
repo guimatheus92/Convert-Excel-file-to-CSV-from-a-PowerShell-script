1d81493d2f8853efd97cc9e5fcba26e758816fe7
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2020-09-09 21:16:56 -0300

    Add files via upload

---
+++ EXCEL_TO_CSV.ps1
@@ -0,0 +1,28 @@
+﻿# Defines the directory and name of the file to be exported to the CSV file
+$dir = "D:\"
+
+# Defines the name of the Excel file
+$excelFileName = "YOUR_FILE_NAME"
+
+# Define a function to convert the file
+Function ExportWSToCSV ($excelFileName, $csvLoc)
+{
+    $excelFile = $dir + $excelFileName + ".xlsx"
+    $E = New-Object -ComObject Excel.Application
+    $E.Visible = $false
+    $E.DisplayAlerts = $false
+    $wb = $E.Workbooks.Open($excelFile)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $n = $excelFileName
+        $ws.SaveAs($csvLoc + $n + ".csv", 6)
+    }
+    $E.Quit()
+}
+
+# For each file in the directory with the xlsx format, convert to CSV using the function above
+$ens = Get-ChildItem $dir -filter *.xlsx
+foreach($e in $ens)
+{
+    ExportWSToCSV -excelFileName $e.BaseName -csvLoc $dir
+}
